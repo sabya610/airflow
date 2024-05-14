865cce04a1161e1c44e40039ab657cc941a2fca4
sabya610 <122990341+sabya610@users.noreply.github.com>
2024-05-14 17:05:04 +0530

    Update sabs-dag-file.py

--- sabs-dag-file.py
+++ sabs-dag-file.py
@@ -7,7 +7,7 @@ local_tz = pendulum.timezone("Asia/kolkata")
 default_args=dict(
     start_date=datetime(2016, 1, 1, tzinfo=local_tz),
     owner='airflow',
-	"start_date"=START_DATE,
+    "start_date"=START_DATE,
 )
 
 
